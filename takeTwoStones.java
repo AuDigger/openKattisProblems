88603ccbbf721cdf3e34e137933f44b5b3e1b424
AuDigger <jarvistrevor1@gmail.com>
2017-03-15 15:40:49 -0400

    setup & first problem

---
+++ takeTwoStones.java
@@ -0,0 +1,17 @@
+package cProgramming;
+
+import java.util.Scanner;
+
+public class takeTwoStones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		if(sc.nextInt()%2==1) System.out.println("Alice");
+		else System.out.println("Bob");
+		
+		
+	}
+
+}
